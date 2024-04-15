c9a30a335256ddcf34cad0e7776d25be1576d87e
Nirbhay-sys <119494283+Nirbhay-sys@users.noreply.github.com>
2024-04-16 05:08:14 +0530

    updated app

--- app.js
+++ app.js
@@ -9,3 +9,29 @@ const thirty = document.getElementById("thirty");
 const sixty = document.getElementById("sixty");
 const beg = document.getElementById("beg");
 const pro = document.getElementById("pro");
+
+var wordNo = 1;
+var wordsSubmitted = 0;
+var wordsCorrect = 0;
+var timer = 30;
+var flag=0;
+var factor=2;
+var seconds;
+var difficulty=1;
+
+displayTest(difficulty);
+
+//on Input
+inputItem.addEventListener('input', function(event){
+  if(flag===0){
+    flag=1;
+    timeStart();
+  }
+  var charEntered = event.data;
+  if(/\s/g.test(charEntered)){  /
+    checkWord();
+  }
+  else{
+    currentWord();
+  }
+});
